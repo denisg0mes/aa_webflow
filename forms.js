0e64b14d5b1e36ee6f60c403b760ca06496e1df7
Denis Gomes <denisgomesmy@gmail.com>
2025-07-28 23:18:43 -0400

    Update forms.js

--- forms.js
+++ forms.js
@@ -13,7 +13,7 @@ function isValidName(name) {
 }
 
 // Toast уведомления
-function showToast(message, isError = false, duration = 4000) {
+function showToast(message, isError = false, duration = 10000) {
     const container = document.getElementById('toastContainer');
     if (!container) {
         console.warn('Toast container not found');
